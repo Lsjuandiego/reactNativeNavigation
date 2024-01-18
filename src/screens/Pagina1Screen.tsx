1ddb3e4a5b116a9606c8c7fd44c9dbacae8de539
lsjuandiego <jlopez@almeraim.com>
2024-01-18 15:43:25 -0500

    asignacion del tipo de dato que pasa como arg

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -23,16 +23,37 @@ export const Pagina1Screen = ({ navigation }: Props) => {
       <Text>
         Navegar con argumentos
       </Text>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('PersonaScreen', {
-            id: 1,
-            nombre: 'Pedro'
-          })
-        }}
+
+      <View
+        style={{ flexDirection: 'row' }}
       >
-        <Text>Pedro</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={{...styles.botonGrande,
+          backgroundColor: '#5856D6'}}
+          onPress={() => {
+            navigation.navigate('PersonaScreen', {
+              id: 1,
+              nombre: 'Pedro'
+            })
+          }}
+        >
+          <Text style={styles.botonGrandeTexto}>Pedro</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{...styles.botonGrande,
+            backgroundColor: '#FF9427'}}
+          onPress={() => {
+            navigation.navigate('PersonaScreen', {
+              id: 2,
+              nombre: 'María'
+            })
+          }}
+        >
+          <Text style={styles.botonGrandeTexto}>María</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
--- src/screens/PersonaScreen.tsx
+++ src/screens/PersonaScreen.tsx
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParams } from '../navigator/StackNavigator';
 
-interface Props extends StackScreenProps<any, any> { };
+interface Props extends StackScreenProps<RootStackParams, 'PersonaScreen'> { };
 
 export const PersonaScreen = ({ route, navigation }: Props) => {
 
@@ -11,7 +12,7 @@ export const PersonaScreen = ({ route, navigation }: Props) => {
 
     useEffect(() => {
         navigation.setOptions({
-            title: params!.nombre
+            title: params.nombre
         })
     }, []);
 
